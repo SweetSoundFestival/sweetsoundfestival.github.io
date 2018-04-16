bdc79cf883dae593489164443f9593a427387e26
hugecat <hugecat@Chieh-MBPR.local>
2018-04-16 23:42:10 +0800

    fix minor problems

--- style.js
+++ style.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 21, 2018 23:59:59").getTime();
+var countDownDate = new Date("June 8, 2018 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
