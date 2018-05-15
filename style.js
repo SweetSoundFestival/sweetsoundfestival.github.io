56fc9e86f16d3127ab9ed7cf2df593b4d1ab41da
hugecat <hugecat@Chieh-MBPR.local>
2018-05-16 02:19:49 +0800

    initial commit

---
+++ style.js
@@ -0,0 +1,40 @@
+// Set the date we're counting down to
+var countDownDate = new Date("June 8, 2018 18:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("day").innerHTML = days ;
+    document.getElementById("hour").innerHTML = hours ;
+    document.getElementById("minute").innerHTML = minutes ;
+    document.getElementById("second").innerHTML = seconds ;
+
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("s1__counter").innerHTML = "開唱啦";
+    }
+}, 1000);
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
