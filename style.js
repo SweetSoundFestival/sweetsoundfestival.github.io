37d31cc4422add747acbc05501509fba4b3ff2c1
hugecat <hugecat@Chieh-MBPR.local>
2018-05-21 21:24:04 +0800

    add a few new functions

--- style.js
+++ style.js
@@ -7,39 +7,6 @@ $(document).ready(function(){
     $.getScript("https://docs.google.com/forms/d/1NtTN7Y9CgedNU5uXQ6mr3_7AUunGKrsdR-bBbGxcT3Q/formResponse?entry.1847439294=" + $("input.q1").val());
   });
 
-  /*if($(document).width > "768px") {
-
-    $("#sj__pic").hover(function() {
-      $("#sj__word").toggle();
-    });
-
-    $("#bug__pic").hover(function() {
-      $("#bug__word").toggle();
-    });
-
-    $("#os__pic").hover(function() {
-      $("#os__word").toggle();
-    });
-
-    $("#byg__pic").hover(function() {
-      $("#byg__word").toggle();
-    });
-
-    $("#egg__pic").hover(function() {
-      $("#egg__word").toggle();
-    });
-
-    $("#cons__pic").hover(function() {
-      $("#cons__word").toggle();
-    });
-
-  }*/
-
-
-
-
-
-
 });
 
 // Set the date we're counting down to
@@ -70,15 +37,90 @@ var x = setInterval(function() {
     // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("s1__counter").innerHTML = "開唱啦";
+        document.getElementById("s1__counter").innerHTML = "府城開唱";
     }
 }, 1000);
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// progressive-image.js
+if (window.addEventListener && window.requestAnimationFrame && document.getElementsByClassName) window.addEventListener('load', function() {
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
+  // start
+  var pItem = document.getElementsByClassName('progressive replace'), timer;
+
+  window.addEventListener('scroll', scroller, false);
+  window.addEventListener('resize', scroller, false);
+  inView();
+
+
+  // throttled scroll/resize
+  function scroller(e) {
+
+    timer = timer || setTimeout(function() {
+      timer = null;
+      requestAnimationFrame(inView);
+    }, 300);
+
+  }
+
+
+  // image in view?
+  function inView() {
+
+    var wT = window.pageYOffset, wB = wT + window.innerHeight, cRect, pT, pB, p = 0;
+    while (p < pItem.length) {
+
+      cRect = pItem[p].getBoundingClientRect();
+      pT = wT + cRect.top;
+      pB = pT + cRect.height;
+
+      if (wT < pB && wB > pT) {
+        loadFullImage(pItem[p]);
+        pItem[p].classList.remove('replace');
+      }
+      else p++;
+
+    }
+
+  }
+
+
+  // replace with full image
+  function loadFullImage(item) {
+
+    if (!item || !item.href) return;
+
+    // load image
+    var img = new Image();
+    if (item.dataset) {
+      img.srcset = item.dataset.srcset || '';
+      img.sizes = item.dataset.sizes || '';
+    }
+    img.src = item.href;
+    img.className = 'reveal';
+    if (img.complete) addImg();
+    else img.onload = addImg;
+
+    // replace image
+    function addImg() {
+
+      // disable click
+      item.addEventListener('click', function(e) { e.preventDefault(); }, false);
+
+      // add full image
+      item.appendChild(img).addEventListener('animationend', function(e) {
+
+        // remove preview image
+        var pImg = item.querySelector && item.querySelector('img.preview');
+        if (pImg) {
+          e.target.alt = pImg.alt || '';
+          item.removeChild(pImg);
+          e.target.classList.remove('reveal');
+        }
+
+      });
+
+    }
+
+  }
+
+}, false);
