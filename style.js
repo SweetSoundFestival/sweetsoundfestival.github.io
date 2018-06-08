acb5c7b98147b3fa6e5bfb3319a1cfc8eb8cd9d8
hugecat <hugecat@Chieh-MBPR.local>
2018-06-08 23:19:21 +0800

    fix timer

--- style.js
+++ style.js
@@ -22,10 +22,16 @@ var x = setInterval(function() {
     var distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
+    /*
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    */
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
 
     // Output the result in an element with id="demo"
     document.getElementById("day").innerHTML = days ;
